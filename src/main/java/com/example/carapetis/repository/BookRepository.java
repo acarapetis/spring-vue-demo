683ff16ac380646596780d9ab287475823b7413c
Anthony Carapetis <anthony.carapetis@gmail.com>
2019-05-20 20:51:43 +1000

    Ensure random book generation terminates

--- src/main/java/com/example/carapetis/repository/BookRepository.java
+++ src/main/java/com/example/carapetis/repository/BookRepository.java
@@ -10,20 +10,31 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface BookRepository extends CrudRepository<Book, Long> {
     public Optional<Book> findById(Long id);
     public List<Book> findAll();
     public Page<Book> findAll(Pageable paging);
     public long count();
 
+    /* 
+     * Get a random book.
+     * 
+     * TODO: depending on the exact behaviour of generated IDs, this could
+     * become very inefficient and even ineffective. The current configuration
+     * assigns sequential IDs starting at 1; but in a serious system we'd want
+     * to confirm this is guaranteed behaviour.
+     */
     public default Optional<Book> findOneRandomly() {
+        int MAX_RETRIES = 100;
+
         long count = this.count();
         if (count <= 0) {
             return Optional.empty();
         }
 
         Optional<Book> book = Optional.empty();
-        while (!book.isPresent()) {
+        for (int i = 0; i < MAX_RETRIES && !book.isPresent(); i++) {
             book = this.findById(ThreadLocalRandom.current().nextLong(count));
         }
         return book;
